ea23b92d3843497a3480e377b5c578411803fae3
5237-mests <msfnmulgeta@gmail.com>
2024-04-24 13:11:41 +0300

    Upcoming meeting reminder being made dynamic on the homepage.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,19 @@
+// @ts-nocheck
+"use client";
 import MeetingTypeList from '@/components/MeetingTypeList';
-import React from 'react'
+import { useGetCalls } from '@/hooks/useGetCalls';
 
 const Home = () => {
+  const { upcomingCalls } = useGetCalls();
+  // I want to sort the upcoming calls by date
+  upcomingCalls.sort((a, b) => new Date(a.state.startsAt) - new Date(b.state.startsAt));
+  // // i want to get the latest meeting
+  const latestMeeting = upcomingCalls[0];
+  const meetingdate = latestMeeting?.state?.startsAt
+
+  const meetingDay = meetingdate?.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' });
+  const meetingTime = meetingdate?.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+
   // CURRENT DATE
   const now = new Date();
   const time = now.toLocaleTimeString( 'en-US', { hour: 'numeric', minute: 'numeric', hour12: true } );
@@ -12,7 +24,10 @@ const Home = () => {
       <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>
-            Upcoming Meeting at: 12:30 PM
+            Upcoming Meeting at:
+            <p>
+              {meetingDay} @ {meetingTime}
+            </p>
           </h2>
           <div className='flex flex-col gap-2'>
             <h1 className='text-4xl font-extrabold lg:text-7xl'>
